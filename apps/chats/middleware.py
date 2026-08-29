9e0209825ef8911264e8eb02d4b8991af2960151
dgek20040104-ops <dgek20040104@gmail.com>
2026-08-29 23:37:59 +0300

    Update middleware.py

--- apps/chats/middleware.py
+++ apps/chats/middleware.py
@@ -2,21 +2,22 @@ from urllib.parse import parse_qs
 
 from channels.db import database_sync_to_async
 from channels.middleware import BaseMiddleware
-from django.contrib.auth import get_user_model
-from django.contrib.auth.models import AnonymousUser
-from rest_framework_simplejwt.exceptions import (
-    TokenError,
-)
-from rest_framework_simplejwt.tokens import (
-    AccessToken,
-)
+# Убираем глобальные импорты моделей и get_user_model
+# from django.contrib.auth import get_user_model
+# from django.contrib.auth.models import AnonymousUser
 
-
-User = get_user_model()
+from rest_framework_simplejwt.exceptions import TokenError
+from rest_framework_simplejwt.tokens import AccessToken
 
 
 @database_sync_to_async
 def get_user_from_token(token):
+    # Импортируем всё необходимое ВНУТРИ функции
+    from django.contrib.auth import get_user_model
+    from django.contrib.auth.models import AnonymousUser
+
+    User = get_user_model()
+
     if not token:
         return AnonymousUser()
 
@@ -42,38 +43,13 @@ def get_user_from_token(token):
         return AnonymousUser()
 
 
-class JWTAuthMiddleware(
-    BaseMiddleware
-):
-    async def __call__(
-        self,
-        scope,
-        receive,
-        send,
-    ):
-        query_string = (
-            scope.get(
-                "query_string",
-                b"",
-            )
-            .decode("utf-8")
-        )
+class JWTAuthMiddleware(BaseMiddleware):
+    async def __call__(self, scope, receive, send):
+        query_string = scope.get("query_string", b"").decode("utf-8")
+        query_params = parse_qs(query_string)
 
-        query_params = parse_qs(
-            query_string,
-        )
+        token = query_params.get("token", [None])
 
-        token = query_params.get(
-            "token",
-            [None],
-        )[0]
+        scope["user"] = await get_user_from_token(token)
 
-        scope["user"] = (
-            await get_user_from_token(token)
-        )
-
-        return await super().__call__(
-            scope,
-            receive,
-            send,
-        )
+        return await super().__call__(scope, receive, send)
