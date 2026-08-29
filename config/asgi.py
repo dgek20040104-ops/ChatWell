f9f854dbfcff28e75a95d393b374e6e3ffa3ec08
dgek20040104-ops <dgek20040104@gmail.com>
2026-08-29 22:13:58 +0300

    Update asgi.py

--- config/asgi.py
+++ config/asgi.py
@@ -5,33 +5,24 @@ os.environ.setdefault(
     "config.settings",
 )
 
+from django.core.asgi import get_asgi_application
+
+# Здесь происходит загрузка приложений — django.setup()
+django_asgi_app = get_asgi_application()
+
+# Все импорты, которые тянут Django-модели, — ТОЛЬКО ПОСЛЕ setup()
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter
 from channels.routing import URLRouter
-from django.core.asgi import (
-    get_asgi_application,
-)
 
-from apps.chats.middleware import (
-    JWTAuthMiddleware,
-)
-
-from apps.chats.routing import (
-    websocket_urlpatterns,
-)
-
-
-django_asgi_app = get_asgi_application()
-
+from apps.chats.middleware import JWTAuthMiddleware
+from apps.chats.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-
         "websocket": JWTAuthMiddleware(
-            URLRouter(
-                websocket_urlpatterns
-            )
+            URLRouter(websocket_urlpatterns)
         ),
     }
 )
