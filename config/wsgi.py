2842c49cd291d065a8de7413e7d2ccdabd45b9a7
dgek20040104-ops <dgek20040104@gmail.com>
2026-08-31 21:57:35 +0300

    Update wsgi.py

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,11 +1,13 @@
 import os
 
-from django.core.wsgi import get_wsgi_application
-
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
     "config.settings",
 )
+
+
+from django.core.wsgi import get_wsgi_application
+
 
 application = get_wsgi_application()
