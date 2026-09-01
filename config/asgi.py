91a7b81a75fbd64c3aa340a7567b61a2bcc9414d
dgek20040104-ops <dgek20040104@gmail.com>
2026-09-01 14:25:17 +0300

    Refactor ASGI configuration and imports

--- config/asgi.py
+++ config/asgi.py
@@ -1,29 +1,35 @@
 import os
 
 
+from channels.routing import (
+    ProtocolTypeRouter,
+    URLRouter,
+)
+
+
+from django.core.asgi import (
+    get_asgi_application,
+)
+
+
+from apps.chats.middleware import (
+    JWTAuthMiddlewareStack,
+)
+
+
+from apps.chats.routing import (
+    websocket_urlpatterns,
+)
+
+
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
     "config.settings",
 )
 
 
-from django.core.asgi import get_asgi_application
-
-
 django_asgi_application = (
     get_asgi_application()
-)
-
-
-from channels.routing import ProtocolTypeRouter
-from channels.routing import URLRouter
-
-
-from apps.chats.jwt_middleware import (
-    JWTAuthMiddlewareStack,
-)
-from apps.chats.routing import (
-    websocket_urlpatterns,
 )
 
 
@@ -33,8 +39,8 @@ application = ProtocolTypeRouter(
 
         "websocket": JWTAuthMiddlewareStack(
             URLRouter(
-                websocket_urlpatterns,
-            ),
+                websocket_urlpatterns
+            )
         ),
-    },
+    }
 )
